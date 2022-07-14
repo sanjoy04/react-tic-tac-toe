c768178c4be063bf9771d5215f7130a2654ff9cc
Sanjoy Roy <sanjoy12k@gmail.com>
2022-07-14 16:28:26 +0530

    title and facicon change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TicTacToe from './TicTacToe';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "Tic-Tac-Toe";
+  });
   return (
     <div className="App">
       <TicTacToe/>
